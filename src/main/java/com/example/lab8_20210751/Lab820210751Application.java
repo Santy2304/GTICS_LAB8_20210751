1cb3cc5c7e77cfa53b92c1a21d27ddc4829bae80
unknown <a20210751@pucp.edu.pe>
2024-06-13 19:21:19 -0500

    inicio del lab

---
+++ src/main/java/com/example/lab8_20210751/Lab820210751Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20210751;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820210751Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820210751Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab8_20210751/Lab820210751ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20210751;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820210751ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
